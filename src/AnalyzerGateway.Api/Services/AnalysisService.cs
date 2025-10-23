883f6803dd9a37426b5d897fe199129acc639ba2
Luciano Amato <luciano2697@outlook.com.ar>
2025-10-23 14:08:11 -0300

    fixed: cssSelector & severity order

--- src/AnalyzerGateway.Api/Services/AnalysisService.cs
+++ src/AnalyzerGateway.Api/Services/AnalysisService.cs
@@ -70,8 +70,8 @@ namespace AnalyzerGateway.Api.Services
                     m.Category,
                     m.Description,
                     m.State,
-                    m.CssSelector,
-                    m.Severity
+                    m.Severity,
+                    m.CssSelector
                     ))
                 .ToList();
 
